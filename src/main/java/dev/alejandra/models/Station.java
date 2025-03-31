24d1d8b26ef7dbdfb5661194b679a16acf7360f6
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 11:40:36 +0200

    "FEAT [String city] implemented to fix failing test"

--- src/main/java/dev/alejandra/models/Station.java
+++ src/main/java/dev/alejandra/models/Station.java
@@ -1,14 +1,20 @@
 package dev.alejandra.models;
 
 public class Station {
-    
+
     String name;
+    String city;
  
-    public Station (String name) {
+    public Station (String name, String city) {
         this.name = name;
+        this.city = city;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
