ca75b4e6bb1b5adb6bcb10bd5730ad50b285c9df
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 13:04:16 +0200

    "FEAT [Motorbike] implemented"

---
+++ src/main/java/dev/alejandra/models/Motorbike.java
@@ -0,0 +1,8 @@
+package dev.alejandra.models;
+
+public class Motorbike extends Vehicle {
+ 
+    public Motorbike(String brand, String licensePlate) {
+        super(brand, licensePlate);
+    }
+}
