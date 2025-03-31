8fd8b46ded97e473588aa06b0399cde0b7857bd9
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 12:14:17 +0200

    "TEST [ShouldReturnLicensePlateOfCar] failed"

--- src/test/java/dev/alejandra/models/VehicleTest.java
+++ src/test/java/dev/alejandra/models/VehicleTest.java
@@ -2,6 +2,8 @@ package dev.alejandra.models;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+
+
 import org.junit.jupiter.api.Test;
 
 public class VehicleTest {
@@ -11,8 +13,19 @@ public class VehicleTest {
  
          String brand = "Chevrolet";
  
-         Car car = new Car("Chevrolet");
+         Car car = new Car(brand);
  
          assertThat(car.getBrand(), is(brand));
+     }
+
+     @Test
+     void testShouldReturnLicensePlateOfCar(){
+ 
+         String brand = "Chevrolet";
+         String licensePlate = "444XAZ";
+ 
+         Car car = new Car(brand, licensePlate);
+ 
+         assertThat(car.getLicensePlate(), is(brand));
      }
 }
