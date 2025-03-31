9cd1451386ca9e86fc92691d542c0fb6ed2ddb5a
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 11:33:53 +0200

    "FEAT [Station] implemented to fix failing tests- String name, constructor and getter"

---
+++ src/main/java/dev/alejandra/models/Station.java
@@ -0,0 +1,14 @@
+package dev.alejandra.models;
+
+public class Station {
+    
+    String name;
+ 
+    public Station (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
