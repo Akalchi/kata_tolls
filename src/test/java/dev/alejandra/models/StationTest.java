49260ca7c05570a4d2527eaa44535f168d8283f6
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 11:31:53 +0200

    "TEST [StationTest, ShouldReturnNameOfStation] failed"

---
+++ src/test/java/dev/alejandra/models/StationTest.java
@@ -0,0 +1,19 @@
+package dev.alejandra.models;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+ 
+ public class StationTest {
+ 
+     @Test
+     
+     void testShouldReturnNameOfStation(){
+ 
+         String name = "Stormwind";
+         
+         Station station = new Station(name);
+ 
+         assertThat(station.getName(), is(name));
+     }
+ }
