077e308ac31e1b22bab964d2448c6e8c7137d433
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 12:25:59 +0200

    "REFACTOR [ShouldReturnAttributesOfVehicles] - all tests in one"

--- src/test/java/dev/alejandra/models/VehicleTest.java
+++ src/test/java/dev/alejandra/models/VehicleTest.java
@@ -2,30 +2,19 @@ package dev.alejandra.models;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
-
 import org.junit.jupiter.api.Test;
 
 public class VehicleTest {
 
-     @Test
-     void testShouldReturnBrandOfCar(){
- 
-         String brand = "Chevrolet";
- 
-         Car car = new Car(brand);
- 
-         assertThat(car.getBrand(), is(brand));
-     }
+    @Test
+    void testShouldReturnAttributesOfVehicles() {
 
-     @Test
-     void testShouldReturnLicensePlateOfCar(){
- 
-         String brand = "Chevrolet";
-         String licensePlate = "444XAZ";
- 
-         Car car = new Car(brand, licensePlate);
- 
-         assertThat(car.getLicensePlate(), is(licensePlate));
-     }
+        String brand = "Chevrolet";
+        String licensePlate = "444XAZ";
+
+        Vehicle car = new Car(brand, licensePlate);
+
+        assertThat(car.getBrand(), is(brand));
+        assertThat(car.getLicensePlate(), is(licensePlate));
+    }
 }
